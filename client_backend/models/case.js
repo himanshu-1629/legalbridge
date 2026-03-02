959cc34ab9be407226742e03347be7588af00237
shreyash2695 <shreyash2695@gmail.com>
2026-03-03 02:35:02 +0530

    Update case.js

--- client_backend/models/case.js
+++ client_backend/models/case.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
 
+const caseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: {
+    type: String,
+    enum: ["ACTIVE", "WAITING", "COMPLETED"],
+    default: "ACTIVE"
+  },
+  progress: {
+    type: Number,
+    default: 0
+  },
+  hearingDate: Date,
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Case", caseSchema);
