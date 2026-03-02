47339ec88ce8f0e999bab58cb994c587e90423a6
shreyash2695 <shreyash2695@gmail.com>
2026-03-03 02:39:04 +0530

    Update server.js

--- client_backend/server.js
+++ client_backend/server.js
@@ -1,1 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const clientRoutes = require("./routes/Client");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/client", clientRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
