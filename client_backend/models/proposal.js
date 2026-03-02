322c69f49bd4ba5fa3faf468e97662956fca8d8f
shreyash2695 <shreyash2695@gmail.com>
2026-03-03 02:36:16 +0530

    Update proposal.js

--- client_backend/models/proposal.js
+++ client_backend/models/proposal.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
 
+const proposalSchema = new mongoose.Schema({
+  caseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Case"
+  },
+  lawyerName: String,
+  status: {
+    type: String,
+    enum: ["PENDING", "ACCEPTED", "REJECTED"],
+    default: "PENDING"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Proposal", proposalSchema);
