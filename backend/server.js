3a1ec48bc0c26d70861c1d85f84c357e8315f39e
himanshu-1629 <himanshugangasingh12@gmail.com>
2026-03-02 21:25:39 +0530

    Refactor server to use environment variable for port

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(express.json());
 const lawyerRoutes = require("./routes/lawyerRoutes");
 app.use("/api/lawyer", lawyerRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
